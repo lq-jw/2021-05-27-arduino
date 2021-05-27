b5e5d545417b37b9ba8897ccc5af15c20c43017d
lq-jw <83740897+lq-jw@users.noreply.github.com>
2021-05-27 11:56:39 +0800

    Add files via upload

---
+++ ans1.ino
@@ -0,0 +1,21 @@
+// C++ code
+//
+int SensorValue = 0;
+
+void setup()
+{
+  pinMode(A0, INPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  SensorValue = analogRead(A0);
+  Serial.println(SensorValue);
+  delay(1);
+  digitalWrite(13, HIGH);
+  delay(SensorValue); // Wait for 1000 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(SensorValue); // Wait for 1000 millisecond(s)
+}
